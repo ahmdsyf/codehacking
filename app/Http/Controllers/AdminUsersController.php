231ac8aa3ebe97fd2d7916e0dbf9a40bfb4a5159
Ahmad <ahmad@gmail.com>
2018-03-17 22:10:13 +0800

    user section working almost done, except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UsersRequest;
+
 use App\User;
 use App\Role;
 use App\Photo;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 
 
 
@@ -54,7 +56,18 @@ class AdminUsersController extends Controller
         // save to datebase
         // User::create($request->all());
 
-        $input = $request->all();
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        }else{
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+        
 
         if($file = $request->file('photo_id')){
 
@@ -66,9 +79,7 @@ class AdminUsersController extends Controller
 
             $input['photo_id'] = $photo->id;
         }
-
-        $input['password'] = bcrypt($request->password);
-
+        
         User::create($input);
 
         return redirect('/admin/users');
@@ -95,7 +106,11 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
@@ -105,9 +120,40 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        }else{
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+        
+
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+        
+        // return $request->all();
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -33,6 +33,7 @@
         </div>
 
         <div class="form-group">
+            {!! Form::label('photo_id', 'Photo') !!}
             {!! Form::file('photo_id', ['class' => 'form-control']) !!}
         </div>
 
